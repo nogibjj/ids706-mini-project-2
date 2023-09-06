5645d5624b7c712fd0b343037bad054dec7e0203
Gunel Aghakishiyeva <78721466+aghakishiyeva@users.noreply.github.com>
2023-09-06 14:25:51 -0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,20 @@
-def greet(name: str) -> str:
-    """Returns a greeting message."""
-    return f"Hello, {name}!"
+import pandas as pd
+from typing import Union, Dict
+
+def describe_data(data: Union[pd.DataFrame, Dict]) -> pd.DataFrame:
+    """Returns the descriptive statistics of the dataset."""
+    
+    if isinstance(data, Dict):
+        data = pd.DataFrame(data)
+    
+    return data.describe()
 
 if __name__ == "__main__":
-    print(greet("world"))
+
+    sample_data = {
+        "A": [1, 2, 3, 4, 5],
+        "B": [5, 4, 3, 2, 1],
+        "C": [2, 3, 4, 3, 2]
+    }
+    df = pd.DataFrame(sample_data)
+    print(describe_data(df))
