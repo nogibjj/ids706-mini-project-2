e6f21a9804b02e2123ee8070cc247be8113821e7
Gunel Aghakishiyeva <78721466+aghakishiyeva@users.noreply.github.com>
2023-09-10 20:55:34 -0400

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,12 +3,12 @@ import pandas as pd
 from src.main import read_dataset, generate_summary_statistics, create_data_visualization
 
 def test_read_dataset():
-    data_csv = read_dataset('test.csv')
+    data_csv = read_dataset('tests/test.csv')
     assert isinstance(data_csv, pd.DataFrame)
     assert not data_csv.empty
 
 def test_generate_summary_statistics():
-    data = read_dataset('test.csv')
+    data = read_dataset('tests/test.csv')
     
     summary = generate_summary_statistics(data)
     assert isinstance(summary, dict)
@@ -25,7 +25,7 @@ def test_create_data_visualization():
     output_dir = 'output'
     os.makedirs(output_dir, exist_ok=True)
     
-    data = read_dataset('test.csv')
+    data = read_dataset('tests/test.csv')
     output_file = 'output/test_data_visualization.png'
     create_data_visualization(data, output_file)
     assert os.path.isfile(output_file)
